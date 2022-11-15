458711de56dcaae174f994921d2c5580bb261ce7
정재현 <33153837+ray1203@users.noreply.github.com>
2022-11-15 20:49:10 +0900

    버튼에 여러 항목 넣을 수 있도록 수정

--- magnet_platform/Assets/Scripts/Obstacles/ButtonObjCtrl.cs
+++ magnet_platform/Assets/Scripts/Obstacles/ButtonObjCtrl.cs
@@ -6,34 +6,80 @@ public class ButtonObjCtrl : MonoBehaviour
 {
     private ButtonTopCol topCol;
     private Transform buttonTop;
-    public enum ButtonSetting
-    {
-        magnet,
-        gate,
-
-    }
-    public ButtonSetting setting;
-    public GameObject attachedObj;
+    
+    public List<GameObject> attachedObjs = new List<GameObject>();
+    private List<ButtonData> buttonDatas = new List<ButtonData>();
     private Vector2 topScale;
-    private delegate void emptyFunc(bool flag);
-    private emptyFunc func;
-    private SpriteRenderer objSprite;
-
-    private MagnetCtrl magnetCtrl;
-    private char magnetism;
-
-    private ButtonGate buttonGate;
     // Start is called before the first frame update
     void Start()
     {
         topCol = GetComponentInChildren<ButtonTopCol>();
         buttonTop = gameObject.transform.Find("Top");
         topScale = buttonTop.localScale;
-        objSprite = attachedObj.GetComponent<SpriteRenderer>();
-        if(setting == ButtonSetting.magnet)
+        
+        for(int i = 0; i < attachedObjs.Count; i++)
         {
-            attachedObj.TryGetComponent<MagnetCtrl>(out magnetCtrl);
-            magnetism = magnetCtrl.magnetism;
+            ButtonData buttonData = new ButtonData();
+            buttonData.objSprite = attachedObjs[i].GetComponent<SpriteRenderer>();
+            if (attachedObjs[i].TryGetComponent<MagnetCtrl>(out MagnetCtrl magnetCtrl))
+            {
+                buttonData.setting = ButtonData.ButtonSetting.magnet;
+                buttonData.magnetCtrl = magnetCtrl;
+                buttonData.magnetism = magnetCtrl.magnetism;
+                
+            }else if (attachedObjs[i].TryGetComponent<ButtonGate>(out ButtonGate buttonGate))
+            {
+                buttonData.setting = ButtonData.ButtonSetting.gate;
+                buttonData.buttonGate = buttonGate;
+            }
+            buttonData.SetFunc();
+            buttonDatas.Add(buttonData);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (topCol.pushing)
+        {
+            float newScaleY = buttonTop.localScale.y - Time.deltaTime;
+            newScaleY = newScaleY > 0.05f ? newScaleY : 0.05f;
+            buttonTop.localScale = new Vector2(topScale.x,newScaleY);
+            buttonTop.transform.localPosition = new Vector2(0, 0.25f + newScaleY / 2.0f);
+            for(int i=0;i<buttonDatas.Count;i++)
+                buttonDatas[i].func(true);
+        }
+        else
+        {
+            float newScaleY = buttonTop.localScale.y + Time.deltaTime;
+            newScaleY = newScaleY <= topScale.y ? newScaleY : topScale.y;
+            buttonTop.localScale = new Vector2(topScale.x, newScaleY);
+            buttonTop.transform.localPosition = new Vector2(0, 0.25f + newScaleY / 2.0f);
+            for (int i = 0; i < buttonDatas.Count; i++)
+                buttonDatas[i].func(false);
+        }
+    }
+    
+}
+public class ButtonData
+{
+    public enum ButtonSetting
+    {
+        magnet,
+        gate,
+    }
+    public ButtonSetting setting;
+    public delegate void emptyFunc(bool flag);
+    public emptyFunc func;
+    public SpriteRenderer objSprite;
+    public MagnetCtrl magnetCtrl;
+    public char magnetism;
+    public ButtonGate buttonGate;
+    public void SetFunc()
+    {
+        if (setting == ButtonSetting.magnet)
+        {
             func = delegate (bool flag)
             {
                 if (flag)
@@ -48,36 +94,12 @@ public class ButtonObjCtrl : MonoBehaviour
                     objSprite.sprite = SpriteManager.instance.whiteTile;
                 }
             };
-        }else if (setting == ButtonSetting.gate)
+        }else if(setting == ButtonSetting.gate)
         {
-            attachedObj.TryGetComponent<ButtonGate>(out buttonGate);
             func = delegate (bool flag)
             {
                 buttonGate.open = flag;
             };
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (topCol.pushing)
-        {
-            float newScaleY = buttonTop.localScale.y - Time.deltaTime;
-            newScaleY = newScaleY > 0.05f ? newScaleY : 0.05f;
-            buttonTop.localScale = new Vector2(topScale.x,newScaleY);
-            buttonTop.transform.localPosition = new Vector2(0, 0.25f + newScaleY / 2.0f);
-            func(true);
-        }
-        else
-        {
-            float newScaleY = buttonTop.localScale.y + Time.deltaTime;
-            newScaleY = newScaleY <= topScale.y ? newScaleY : topScale.y;
-            buttonTop.localScale = new Vector2(topScale.x, newScaleY);
-            buttonTop.transform.localPosition = new Vector2(0, 0.25f + newScaleY / 2.0f);
-
-            func(false);
-        }
-    }
-    
 }
