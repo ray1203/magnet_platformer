3d496dafc5a4564ccd570a0b7ce57dd464326cd9
정재현 <33153837+ray1203@users.noreply.github.com>
2022-11-18 00:35:19 +0900

    gravityArea 버그 수정

--- magnet_platform/Assets/Scripts/Obstacles/GravityArea.cs
+++ magnet_platform/Assets/Scripts/Obstacles/GravityArea.cs
@@ -10,7 +10,8 @@ public class GravityArea : MonoBehaviour
         if(collision.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigid)){
             rigid.gravityScale *= amount;
             rigid.mass *= amount;
-            GameManager.instance.playerMove.MultiplyJumpPower(amount);
+            if(collision.CompareTag("Player"))
+                GameManager.instance.playerMove.MultiplyJumpPower(amount);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -19,7 +20,8 @@ public class GravityArea : MonoBehaviour
         {
             rigid.gravityScale /= amount;
             rigid.mass /= amount;
-            GameManager.instance.playerMove.MultiplyJumpPower(1.0f/amount);
+            if (collision.CompareTag("Player"))
+                GameManager.instance.playerMove.MultiplyJumpPower(1.0f/amount);
         }
     }
 }
